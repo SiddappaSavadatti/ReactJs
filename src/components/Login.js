5aa81422d7854a41e5ae1c55a40d7fad0f8826a0
FirstProjectDemo <siddusavadatti77@gmail.com>
2024-10-26 11:37:17 +0530

    added one h2 tag

--- src/components/Login.js
+++ src/components/Login.js
@@ -56,6 +56,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-box">
+        <h2>Welcome</h2>
         <h2>LOGIN PLEASE</h2>
         <form onSubmit={handleSubmit} className="login-form">
           <div className="input-group">

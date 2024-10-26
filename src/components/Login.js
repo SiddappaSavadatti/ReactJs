e397202b385a3775cd5eb197bd84585231cb22b6
FirstProjectDemo <siddusavadatti77@gmail.com>
2024-10-26 10:41:07 +0530

    change in login.js h2 tag

--- src/components/Login.js
+++ src/components/Login.js
@@ -56,7 +56,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-box">
-        <h2>Please Login</h2>
+        <h2>LOGIN</h2>
         <form onSubmit={handleSubmit} className="login-form">
           <div className="input-group">
             <label htmlFor="username">Username:</label>

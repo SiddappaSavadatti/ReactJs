ab0f8bb31e24fe53632ab7d3e617175ca862b560
Siddappa Savadatti <siddusavadatti77@gmail.com>
2024-10-26 10:31:03 +0530

    Update Login.js
    updated in login.js file

--- src/components/Login.js
+++ src/components/Login.js
@@ -56,7 +56,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-box">
-        <h2>Welcome to Dhanush Hospital</h2>
+        <h2>Welcome to My Hospital</h2>
         <form onSubmit={handleSubmit} className="login-form">
           <div className="input-group">
             <label htmlFor="username">Username:</label>
